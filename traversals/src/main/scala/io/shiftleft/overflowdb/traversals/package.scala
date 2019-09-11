a88ccc99f230d4c14cce7f7b3bb0623982959858
Michael Pollmeier <michael@michaelpollmeier.com>
2019-09-11 15:39:22 +1200

    add graph-generic `out` step

--- traversals/src/main/scala/io/shiftleft/overflowdb/traversals/package.scala
+++ traversals/src/main/scala/io/shiftleft/overflowdb/traversals/package.scala
@@ -1,13 +1,23 @@
 package io.shiftleft.overflowdb
 
+import java.util
+
+import org.apache.tinkerpop.gremlin.structure.Direction
+
+import scala.collection.IterableOnce
+import scala.jdk.CollectionConverters._
+
 package object traversals {
 
   implicit class NodeTraversal[+A <: NodeRef[_]](val traversal: Traversal[A]) extends AnyVal {
     def id: Traversal[Long] = traversal.map(_.id)
+
     def label: Traversal[String] = traversal.map(_.label)
 
     def property[P](key: String): Traversal[P] = traversal.map(_.value[P](key))
+
     def hasProperty(key: String): Traversal[A] = traversal.filter(_.property(key).isPresent)
+
     def hasProperty[P](key: String, value: String): Traversal[A] =
       traversal.filter { node =>
         val property = node.property[P](key)
@@ -15,12 +25,29 @@ package object traversals {
       }
 
     /** Note: do not use as the first step in a traversal, e.g. `traversalSource.all.id(value)`.
-     *  Use `traversalSource.withId` instead, it is much faster */
+     * Use `traversalSource.withId` instead, it is much faster */
     def id(value: Long): Traversal[A] = traversal.filter(_.id == value)
 
     /** Note: do not use as the first step in a traversal, e.g. `traversalSource.all.label(value)`.
-     *  Use `traversalSource.withLabel` instead, it is much faster */
+     * Use `traversalSource.withLabel` instead, it is much faster */
     def label(value: String): Traversal[A] = traversal.filter(_.label == value)
+
+    def out[B <: NodeRef[_]](label: String): Traversal[B] =
+      nodes[B](Direction.OUT, label)
+
+    private def nodes[B <: NodeRef[_]](direction: Direction, label: String): Traversal[B] =
+      traversal.flatMap(_.vertices(direction, label).cast[B].asScala)
+  }
+
+  implicit class JIterableOps[A](val iterable: java.util.Iterator[A]) extends AnyVal {
+    def cast[B]: java.util.Iterator[B] = new java.util.Iterator[B] {
+      override def hasNext = iterable.hasNext
+      override def next() = iterable.next.asInstanceOf[B]
+    }
+  }
+
+  implicit class IterableOnceOps[A](val iterable: IterableOnce[A]) extends AnyVal {
+    def cast[B]: IterableOnce[B] = iterable.iterator.map(_.asInstanceOf[B])
   }
 
 }
