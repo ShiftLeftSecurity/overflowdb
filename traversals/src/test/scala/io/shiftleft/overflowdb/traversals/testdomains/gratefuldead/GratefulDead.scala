58fedc26d1408950c6c68aed99cea0eee5a23f23
Michael Pollmeier <michael@michaelpollmeier.com>
2019-09-18 17:35:16 +1200

    more implicit value classes

--- traversals/src/test/scala/io/shiftleft/overflowdb/traversals/testdomains/gratefuldead/GratefulDead.scala
+++ traversals/src/test/scala/io/shiftleft/overflowdb/traversals/testdomains/gratefuldead/GratefulDead.scala
@@ -2,10 +2,11 @@ package io.shiftleft.overflowdb.traversals.testdomains.gratefuldead
 
 import java.util
 
+import io.shiftleft.overflowdb.traversals.{Traversal, TraversalSource}
 import io.shiftleft.overflowdb.{OdbConfig, OdbGraph}
 import org.apache.tinkerpop.gremlin.structure.io.IoCore
 
-/* schema: https://raw.githubusercontent.com/apache/tinkerpop/347eb9a8231c48aa22eced9b07dd6241305898c6/docs/static/images/grateful-dead-schema.png */
+/* visual schema: https://raw.githubusercontent.com/apache/tinkerpop/347eb9a8231c48aa22eced9b07dd6241305898c6/docs/static/images/grateful-dead-schema.png */
 object GratefulDead {
   def newGraph: OdbGraph = newGraph(OdbConfig.withoutOverflow)
 
@@ -24,4 +25,9 @@ object GratefulDead {
     graph.io(IoCore.graphml).readGraph("src/test/resources/grateful-dead.xml")
 
   def traversal(graph: OdbGraph) = new GratefulDeadTraversalSource(graph)
+}
+
+class GratefulDeadTraversalSource(graph: OdbGraph) extends TraversalSource(graph) {
+  def artists: Traversal[Artist] = withLabelTyped(Artist.Label)
+  def songs: Traversal[Song] = withLabelTyped(Song.Label)
 }
--- traversals/src/test/scala/io/shiftleft/overflowdb/traversals/testdomains/gratefuldead/package.scala
+++ traversals/src/test/scala/io/shiftleft/overflowdb/traversals/testdomains/gratefuldead/package.scala
@@ -4,11 +4,22 @@ import io.shiftleft.overflowdb.traversals.Traversal
 
 package object gratefuldead {
 
-  implicit def traversalToArtistTraversal(trav: Traversal[Artist]): ArtistTraversal =
-    new ArtistTraversal(trav)
-  
-  implicit def traversalToSongTraversal(trav: Traversal[Song]): SongTraversal =
-    new SongTraversal(trav)
+  implicit class ArtistTraversal(val trav: Traversal[Artist]) extends AnyVal {
+    def name: Traversal[String] = trav.map(_.name)
+    def name(value: String): Traversal[Artist] = trav.filter(_.name == value)
+
+    def sangSongs: Traversal[Song] = trav.flatMap(_.sangSongs)
+  }
+
+  implicit class SongTraversal(val trav: Traversal[Song]) extends AnyVal {
+    def name: Traversal[String] = trav.map(_.name)
+    def name(value: String): Traversal[Song] = trav.filter(_.name == value)
+
+    def songType: Traversal[String] = trav.map(_.songType)
+    def performances: Traversal[Int] = trav.map(_.performances)
+
+    def followedBy: Traversal[Song] = trav.flatMap(_.followedBy)
+  }
 
   // TODO move to overflowdb-traversal
   // TODO can we just inherit Traversal and make it an `implicit class <: AnyVal`?
