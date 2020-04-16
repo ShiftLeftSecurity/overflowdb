81adacfbf9628498a9e9ddcda2900012cdcc97c3
Michael Pollmeier <michael@michaelpollmeier.com>
2020-04-16 22:23:47 +1200

    make ArrayOffsetIterator null-aware (i.e. filter `null` values out) (#79)

--- tinkerpop3/src/main/java/io/shiftleft/overflowdb/util/ArrayOffsetIterator.java
+++ tinkerpop3/src/main/java/io/shiftleft/overflowdb/util/ArrayOffsetIterator.java
@@ -1,12 +1,16 @@
 package io.shiftleft.overflowdb.util;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class ArrayOffsetIterator<T> implements Iterator<T> {
   private final Object[] array;
   private int current;
   private final int exclusiveEnd;
   private final int strideSize;
+
+  /** used for peeking */
+  private T nextCached;
 
   public ArrayOffsetIterator(Object[] array, int begin, int exclusiveEnd, int strideSize) {
     this.array = array;
@@ -16,14 +20,36 @@ public class ArrayOffsetIterator<T> implements Iterator<T> {
   }
 
   @Override
-  public boolean hasNext() {
-    return current < exclusiveEnd;
+  public final boolean hasNext() {
+    return peekNext() != null;
+  }
+
+  /**
+   * Elements within the arrays may be null, e.g. because an edge was deleted.
+   * Therefor we need to have the ability to peek forward to the next element.
+  */
+  private T peekNext() {
+    if (nextCached != null)
+      return nextCached;
+
+    if (current < exclusiveEnd) {
+      nextCached = (T) array[current];
+      // n.b. nextCached may be `null`, e.g. if an edge was deleted
+      current += strideSize;
+      return peekNext();
+    }
+
+    return null; // i.e. we have reached the end of this Iterator
   }
 
   @Override
-  public T next() {
-    T element = (T) array[current];
-    current += strideSize;
-    return element;
+  public final T next() {
+    if (hasNext()) {
+      final T ret = peekNext();
+      nextCached = null;
+      return ret;
+    } else {
+      throw new NoSuchElementException();
+    }
   }
 }
