8c5e3833587a1aaf810181e7bf4d739ece1920fa
Michael Pollmeier <michael@michaelpollmeier.com>
2020-04-28 16:29:58 +1200

    define thread name for referencemanager, so we can see it in threaddump

---
+++ tinkerpop3/src/main/java/io/shiftleft/overflowdb/util/NamedThreadFactory.java
@@ -0,0 +1,15 @@
+package io.shiftleft.overflowdb.util;
+
+import java.util.concurrent.ThreadFactory;
+
+public class NamedThreadFactory implements ThreadFactory {
+  private final String threadName;
+
+  public NamedThreadFactory(String threadName) {
+    this.threadName = threadName;
+  }
+
+  public Thread newThread(Runnable r) {
+    return new Thread(r, threadName);
+  }
+}
