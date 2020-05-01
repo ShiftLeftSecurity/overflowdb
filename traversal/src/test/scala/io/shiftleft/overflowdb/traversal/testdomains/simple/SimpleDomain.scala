7d860366ff816b6b367fc4ada25d3be7ae25f513
Michael Pollmeier <michael@michaelpollmeier.com>
2020-05-01 17:06:02 +1200

    RFC prototype for TraversalSource.help

--- traversal/src/test/scala/io/shiftleft/overflowdb/traversal/testdomains/simple/SimpleDomain.scala
+++ traversal/src/test/scala/io/shiftleft/overflowdb/traversal/testdomains/simple/SimpleDomain.scala
@@ -1,8 +1,10 @@
 package io.shiftleft.overflowdb.traversal.testdomains.simple
 
-import io.shiftleft.overflowdb.traversal.{Traversal, TraversalSource}
+import io.shiftleft.overflowdb.traversal.{Traversal, TraversalSource, help}
 import io.shiftleft.overflowdb.{OdbConfig, OdbGraph}
 import java.util
+
+import io.shiftleft.overflowdb.traversal.help.{Doc, TraversalHelp}
 
 /** probably the most simple domain one can think of:
  * Thing --- Connection --> Thing
@@ -16,8 +18,16 @@ object SimpleDomain {
       util.Arrays.asList(Connection.factory))
 
   def traversal(graph: OdbGraph) = new SimpleDomainTraversalSource(graph)
+
+  lazy val help = new TraversalHelp(getClass.getPackage.getName)
 }
 
+@help.TraversalSource
 class SimpleDomainTraversalSource(graph: OdbGraph) extends TraversalSource(graph) {
+
+  @Doc("all things")
   def things: Traversal[Thing] = label(Thing.Label).cast[Thing]
+
+  lazy val help: String = SimpleDomain.help.forSources
 }
+
