534b1019efa1c438cfeffa3c55ffc5cead83977b
Michael Pollmeier <michael@michaelpollmeier.com>
2020-05-25 15:16:29 +1200

    graph + (label, properties)

--- traversal/src/main/scala/io/shiftleft/overflowdb/SyntacticSugar.scala
+++ traversal/src/main/scala/io/shiftleft/overflowdb/SyntacticSugar.scala
@@ -1,8 +1,23 @@
 package io.shiftleft.overflowdb
 
+import io.shiftleft.overflowdb.traversal.PropertyKeyValue
+
 class GraphSugar(val wrapped: OdbGraph) extends AnyVal {
   def `+`(label: String): NodeRef[_] =
     wrapped.addNode(label)
+
+  def `+`(label: String, properties: PropertyKeyValue[_]*): NodeRef[_] =
+    wrapped.addNode(label, keyValuesAsSeq(properties): _*)
+
+  private def keyValuesAsSeq(properties: Seq[PropertyKeyValue[_]]): Seq[_] = {
+    val builder = Seq.newBuilder[Any]
+    builder.sizeHint(properties.size * 2)
+    properties.foreach { kv =>
+      builder += kv.key.name
+      builder += kv.value
+    }
+    builder.result
+  }
 }
 
 class NodeRefSugar(val wrapped: NodeRef[_]) extends AnyVal {
