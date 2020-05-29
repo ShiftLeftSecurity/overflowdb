81350611dca416ad8ce2addb38e134b694e8640c
Michael Pollmeier <michael@michaelpollmeier.com>
2020-05-29 13:06:14 +1200

    refactor: separat out generic graph traversal tests

--- traversal/src/test/scala/io/shiftleft/overflowdb/traversal/TraversalTests.scala
+++ traversal/src/test/scala/io/shiftleft/overflowdb/traversal/TraversalTests.scala
@@ -7,7 +7,6 @@ import org.scalatest.{Matchers, WordSpec}
 
 import scala.collection.mutable
 
-/** generic traversal steps (mostly domain independent) */
 class TraversalTests extends WordSpec with Matchers {
 
   "can only be iterated once" in {
@@ -42,87 +41,6 @@ class TraversalTests extends WordSpec with Matchers {
     simpleDomain.all.property(Thing.Properties.Name).toSet shouldBe Set("L3", "L2", "L1", "Center", "R1", "R2", "R3", "R4")
     centerTrav.head.name shouldBe "Center"
     simpleDomain.all.label.toSet shouldBe Set(Thing.Label)
-  }
-
-  "generic graph steps" can {
-
-    "step out" in {
-      assertNames(centerTrav.out, Set("L1", "R1"))
-      assertNames(centerNode.out, Set("L1", "R1"))
-      assertNames(centerTrav.out.out, Set("L2", "R2"))
-      assertNames(centerNode.out.out, Set("L2", "R2"))
-      assertNames(centerTrav.out(Connection.Label), Set("L1", "R1"))
-      assertNames(centerNode.out(Connection.Label), Set("L1", "R1"))
-      assertNames(centerTrav.out(nonExistingLabel), Set.empty)
-      assertNames(centerNode.out(nonExistingLabel), Set.empty)
-    }
-
-    "step in" in {
-      l2Trav.in.size shouldBe 1
-      l2Node.in.size shouldBe 1
-      assertNames(l2Trav.in, Set("L1"))
-      assertNames(l2Node.in, Set("L1"))
-      assertNames(l2Trav.in.in, Set("Center"))
-      assertNames(l2Node.in.in, Set("Center"))
-      assertNames(l2Trav.in(Connection.Label), Set("L1"))
-      assertNames(l2Node.in(Connection.Label), Set("L1"))
-      assertNames(l2Trav.in(nonExistingLabel), Set.empty)
-      assertNames(l2Node.in(nonExistingLabel), Set.empty)
-    }
-
-    "step both" in {
-      /* L3 <- L2 <- L1 <- Center -> R1 -> R2 -> R3 -> R4 */
-      l2Trav.both.size shouldBe 2
-      l2Node.both.size shouldBe 2
-      assertNames(l2Trav.both, Set("L1", "L3"))
-      assertNames(l2Node.both, Set("L1", "L3"))
-      assertNames(r2Trav.both, Set("R1", "R3"))
-      assertNames(r2Node.both, Set("R1", "R3"))
-      assertNames(l2Trav.both.both, Set("L2", "Center"))
-      assertNames(l2Node.both.both, Set("L2", "Center"))
-      assertNames(r2Trav.both.both, Set("Center", "R2", "R4"))
-      assertNames(r2Node.both.both, Set("Center", "R2", "R4"))
-      assertNames(l2Trav.both(Connection.Label), Set("L1", "L3"))
-      assertNames(l2Node.both(Connection.Label), Set("L1", "L3"))
-      assertNames(l2Trav.both(nonExistingLabel), Set.empty)
-      assertNames(l2Node.both(nonExistingLabel), Set.empty)
-    }
-
-    "step outE" in {
-      centerTrav.outE.size shouldBe 2
-      centerNode.outE.size shouldBe 2
-      assertNames(centerTrav.outE.inV, Set("L1", "R1"))
-      assertNames(centerNode.outE.inV, Set("L1", "R1"))
-      assertNames(centerTrav.outE.inV.outE.inV, Set("L2", "R2"))
-      assertNames(centerNode.outE.inV.outE.inV, Set("L2", "R2"))
-      assertNames(centerTrav.outE(Connection.Label).inV, Set("L1", "R1"))
-      assertNames(centerNode.outE(Connection.Label).inV, Set("L1", "R1"))
-      assertNames(centerTrav.outE(nonExistingLabel).inV, Set.empty)
-      assertNames(centerNode.outE(nonExistingLabel).inV, Set.empty)
-    }
-
-    "step inE" in {
-      l2Trav.inE.size shouldBe 1
-      l2Node.inE.size shouldBe 1
-      assertNames(l2Trav.inE.outV, Set("L1"))
-      assertNames(l2Node.inE.outV, Set("L1"))
-      assertNames(l2Trav.inE.outV.inE.outV, Set("Center"))
-      assertNames(l2Node.inE.outV.inE.outV, Set("Center"))
-      assertNames(l2Trav.inE(Connection.Label).outV, Set("L1"))
-      assertNames(l2Node.inE(Connection.Label).outV, Set("L1"))
-      assertNames(l2Trav.inE(nonExistingLabel).outV, Set.empty)
-      assertNames(l2Node.inE(nonExistingLabel).outV, Set.empty)
-    }
-
-    "step bothE" in {
-      /* L3 <- L2 <- L1 <- Center -> R1 -> R2 -> R3 -> R4 */
-      l2Trav.bothE.size shouldBe 2
-      l2Node.bothE.size shouldBe 2
-      l2Trav.bothE(Connection.Label).size shouldBe 2
-      l2Node.bothE(Connection.Label).size shouldBe 2
-      l2Trav.bothE(nonExistingLabel).size shouldBe 0
-      l2Node.bothE(nonExistingLabel).size shouldBe 0
-    }
   }
 
   "repeat" should {
