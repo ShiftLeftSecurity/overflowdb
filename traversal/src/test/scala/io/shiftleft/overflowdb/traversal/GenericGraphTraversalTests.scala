1afd6a684b986f866fb672c64525839946cc1b6c
Michael Pollmeier <michael@michaelpollmeier.com>
2020-05-29 13:14:31 +1200

    factor out test setup for reuse and conciseness

--- traversal/src/test/scala/io/shiftleft/overflowdb/traversal/GenericGraphTraversalTests.scala
+++ traversal/src/test/scala/io/shiftleft/overflowdb/traversal/GenericGraphTraversalTests.scala
@@ -1,14 +1,12 @@
 package io.shiftleft.overflowdb.traversal
 
 import io.shiftleft.overflowdb.NodeRef
-import io.shiftleft.overflowdb.traversal.testdomains.simple.{Connection, SimpleDomain, SimpleDomainTraversalSource, Thing}
-import org.apache.tinkerpop.gremlin.structure.{T, Vertex}
+import io.shiftleft.overflowdb.traversal.testdomains.simple.{Connection, ExampleGraphSetup, Thing}
 import org.scalatest.{Matchers, WordSpec}
-
-import scala.collection.mutable
 
 /** generic graph traversals, i.e. domain independent */
 class GenericGraphTraversalTests extends WordSpec with Matchers {
+  import ExampleGraphSetup._
 
   "step out" in {
     assertNames(centerTrav.out, Set("L1", "R1"))
@@ -88,43 +86,7 @@ class GenericGraphTraversalTests extends WordSpec with Matchers {
     l2Node.bothE(nonExistingLabel).size shouldBe 0
   }
 
-  val nonExistingLabel = "this label does not exist"
-
-  def simpleDomain: SimpleDomainTraversalSource = SimpleDomain.traversal(simpleGraph)
-  def centerTrav: Traversal[Thing] = simpleDomain.things.name("Center")
-  def centerNode: Thing = centerTrav.head
-  def l2Trav: Traversal[Thing] = simpleDomain.things.name("L2")
-  def l2Node: Thing = l2Trav.head
-  def r2Trav: Traversal[Thing] = simpleDomain.things.name("R2")
-  def r2Node: Thing = r2Trav.head
-
   def assertNames[A <: NodeRef[_]](traversal: Traversal[A], expectedNames: Set[String]) = {
     traversal.property(Thing.Properties.Name).toSet shouldBe expectedNames
-  }
-
-  /* L3 <- L2 <- L1 <- Center -> R1 -> R2 -> R3 -> R4 */
-  lazy val simpleGraph = {
-    val graph = SimpleDomain.newGraph
-
-    def addThing(name: String): Vertex =
-      graph.addVertex(T.label, Thing.Label, Thing.PropertyNames.Name, name)
-
-    val center = addThing("Center")
-    val l1 = addThing("L1")
-    val r1 = addThing("R1")
-    val l2 = addThing("L2")
-    val r2 = addThing("R2")
-    val l3 = addThing("L3")
-    val r3 = addThing("R3")
-    val r4 = addThing("R4")
-
-    center.addEdge(Connection.Label, l1)
-    l1.addEdge(Connection.Label, l2)
-    l2.addEdge(Connection.Label, l3)
-    center.addEdge(Connection.Label, r1)
-    r1.addEdge(Connection.Label, r2)
-    r2.addEdge(Connection.Label, r3)
-    r3.addEdge(Connection.Label, r4)
-    graph
   }
 }
