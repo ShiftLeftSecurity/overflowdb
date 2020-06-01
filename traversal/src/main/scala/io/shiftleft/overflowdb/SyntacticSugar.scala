921349805d711f75092f2afee00948efbbe9417c
Michael Pollmeier <michael@michaelpollmeier.com>
2020-06-02 09:20:44 +1200

    WIP

--- traversal/src/main/scala/io/shiftleft/overflowdb/SyntacticSugar.scala
+++ traversal/src/main/scala/io/shiftleft/overflowdb/SyntacticSugar.scala
@@ -30,7 +30,9 @@ class NodeRefSugar(val node: NodeRef[_]) extends AnyVal {
 }
 
 private[overflowdb] class SemiEdge(outNode: NodeRef[_], label: String, properties: Seq[PropertyKeyValue[_]]) {
-  def -->(inNode: NodeRef[_]): OdbEdge =
+  def -->(inNode: NodeRef[_]): OdbEdge = {
+    val tinkerpopProps = new Array[AnyRef](properties.size * 2)
     outNode.addEdge(label, inNode).asInstanceOf[OdbEdge]
+  }
 }
 
