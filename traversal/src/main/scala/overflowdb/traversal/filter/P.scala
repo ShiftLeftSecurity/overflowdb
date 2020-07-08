404c655a99d6a6aaea05f36b7e34cd210e23110a
Michael Pollmeier <michael@michaelpollmeier.com>
2020-07-09 09:05:54 +1200

    filter elements by a property with arbitrary predicate - and predefine some in `P` (#126)
    * bring in the P concept into odb: simply an `A => Boolean` predicate
    
    * same for hasNot - some cleanup

---
+++ traversal/src/main/scala/overflowdb/traversal/filter/P.scala
@@ -0,0 +1,26 @@
+package overflowdb.traversal.filter
+
+/** commonly used predicates e.g. for Traversal.has|hasNot|is steps */
+object P {
+  def eq[A](a: A): A => Boolean =
+    a.==
+
+  def neq[A](a: A): A => Boolean =
+    a.!=
+
+  def regexMatches(regex: String): String => Boolean =
+    _.matches(regex)
+
+  def within[A](values: Set[A]): A => Boolean =
+    values.contains
+
+  def within[A](values: A*): A => Boolean =
+    within(values.to(Set))
+
+  def without[A](values: Set[A]): A => Boolean =
+    { a: A => !values.contains(a) }
+
+  def without[A](values: A*): A => Boolean =
+    without(values.to(Set))
+
+}
