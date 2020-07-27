12cee67e05f41b5c8a81473d9329e8e3cfde5012
Michael Pollmeier <michael@michaelpollmeier.com>
2020-07-27 20:02:41 +1200

    repeat step: take Traversal[A] => Traversal[B] (#140)
    for consistency with other steps, and to preserve tinkerpop semantics

--- traversal/src/main/scala/overflowdb/traversal/Traversal.scala
+++ traversal/src/main/scala/overflowdb/traversal/Traversal.scala
@@ -157,11 +157,11 @@ class Traversal[A](elements: IterableOnce[A])
    *
    * @see RepeatTraversalTests for more detail and examples for all of the above.
    */
-  final def repeat[B >: A](repeatTraversal: A => Traversal[B])
+  final def repeat[B >: A](repeatTraversal: Traversal[A] => Traversal[B])
     (implicit behaviourBuilder: RepeatBehaviour.Builder[B] => RepeatBehaviour.Builder[B] = RepeatBehaviour.noop[B] _)
     : Traversal[B] = {
     val behaviour = behaviourBuilder(new RepeatBehaviour.Builder[B]).build
-    val _repeatTraversal = repeatTraversal.asInstanceOf[B => Traversal[B]] //this cast usually :tm: safe, because `B` is a supertype of `A`
+    val _repeatTraversal = repeatTraversal.asInstanceOf[Traversal[B] => Traversal[B]] //this cast usually :tm: safe, because `B` is a supertype of `A`
     flatMap(RepeatStep(_repeatTraversal, behaviour))
   }
 
