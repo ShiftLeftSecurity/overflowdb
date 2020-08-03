b3f4c4300c6e98e6763348f00ddf06702eda7d46
Michael Pollmeier <michael@michaelpollmeier.com>
2020-08-04 09:06:59 +1200

    TraversalSource from (potentially) indexed properties (#142)

---
+++ traversal/src/test/scala/overflowdb/traversal/TraversalSourceTest.scala
@@ -0,0 +1,27 @@
+package overflowdb.traversal
+
+import org.scalatest.{Matchers, WordSpec}
+import overflowdb._
+import overflowdb.traversal.testdomains.simple.{SimpleDomain, Thing}
+
+class TraversalSourceTest extends WordSpec with Matchers {
+
+  "property lookup with and without indexes" in {
+    val graph = SimpleDomain.newGraph
+    import Thing.Properties.Name
+
+    val one = graph + (Thing.Label, Name -> "one")
+    val two1 = graph + (Thing.Label, Name -> "two")
+    val two2 = graph + (Thing.Label, Name -> "two")
+
+    def verify() = {
+      TraversalSource(graph).has(Name -> "one").toSet shouldBe Set(one)
+      TraversalSource(graph).has(Name -> "two").toSet shouldBe Set(two1, two2)
+    }
+
+    verify()
+    graph.indexManager.createNodePropertyIndex(Name.name)
+    verify()
+  }
+
+}
