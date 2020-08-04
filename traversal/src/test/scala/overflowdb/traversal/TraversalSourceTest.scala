bfa20f171064dbc61fe88c6250a27d370c9ecfda
Michael Pollmeier <michael@michaelpollmeier.com>
2020-08-04 21:49:20 +1200

    TraversalSource: start with label and property, choose best case (#143)
    * Inspects the cardinality of the indices of the properties and labels, and takes the smaller one

--- traversal/src/test/scala/overflowdb/traversal/TraversalSourceTest.scala
+++ traversal/src/test/scala/overflowdb/traversal/TraversalSourceTest.scala
@@ -17,6 +17,11 @@ class TraversalSourceTest extends WordSpec with Matchers {
     def verify() = {
       TraversalSource(graph).has(Name -> "one").toSet shouldBe Set(one)
       TraversalSource(graph).has(Name -> "two").toSet shouldBe Set(two1, two2)
+      TraversalSource(graph).has(Name -> "unknown").toSet shouldBe Set.empty
+
+      TraversalSource(graph).labelAndProperty(Thing.Label, Name -> "two").toSet shouldBe Set(two1, two2)
+      TraversalSource(graph).labelAndProperty(Thing.Label, Name -> "unknown").toSet shouldBe Set.empty
+      TraversalSource(graph).labelAndProperty("unknown", Name -> "two").toSet shouldBe Set.empty
     }
 
     verify()
