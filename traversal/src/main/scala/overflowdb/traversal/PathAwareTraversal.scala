734edf1c0347dcfe35ee21b62521a8fd5ab410bd
Michael Pollmeier <michael@michaelpollmeier.com>
2020-08-10 14:27:07 +1200

    reimplement flatMap, much simpler

--- traversal/src/main/scala/overflowdb/traversal/PathAwareTraversal.scala
+++ traversal/src/main/scala/overflowdb/traversal/PathAwareTraversal.scala
@@ -10,49 +10,12 @@ import scala.reflect.ClassTag
 class PathAwareTraversal[A](val elementsWithPath: IterableOnce[(A, Vector[Any])]) extends Traversal[A](elementsWithPath.iterator.map(_._1)) {
 //  println("PathAwareTraversal:init")
 
-// idea: implement flatmap ourselves, to avoid conversion to/from iterator
-// initially copied from Iterator.flatMap
-  override def flatMap[B](f: A => IterableOnce[B]): Traversal[B] = {
-    val outerTraversal = this
-    val newIter = new Iterator[(B, Vector[Any])] {
-      private[this] var cur: Traversal[B] = Traversal.empty
-      private[this] var _hasNext: Int = -1
-      /** Trillium logic boolean: -1 = unknown, 0 = false, 1 = true */
-      private[this] var path: Vector[Any] = Vector.empty
-
-      private[this] def nextCur(): Unit = {
-        cur = null
-        val (a, _path) = outerTraversal.elementsWithPath.iterator.next
-        path = _path.appended(a)
-        cur = f(a)
-        _hasNext = -1
+  override def flatMap[B](f: A => IterableOnce[B]): Traversal[B] =
+    new PathAwareTraversal(
+      elementsWithPath.iterator.flatMap { case (a, path) =>
+        f(a).map(b => (b, path.appended(a)))
       }
-
-      def hasNext: Boolean = {
-        if (_hasNext == -1) {
-          while (!cur.hasNext) {
-            if (!outerTraversal.hasNext) {
-              _hasNext = 0
-              cur = Traversal.empty
-              return false
-            }
-            nextCur()
-          }
-          _hasNext = 1
-          true
-        } else _hasNext == 1
-      }
-
-      def next(): (B, Vector[Any]) = {
-        if (hasNext) {
-          _hasNext = -1
-        }
-        (cur.next, path)
-      }
-    }
-
-    new PathAwareTraversal[B](newIter)
-  }
+    )
 
   override def map[B](f: A => B): Traversal[B] =
     new PathAwareTraversal(
