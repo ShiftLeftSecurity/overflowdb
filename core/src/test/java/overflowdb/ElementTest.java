16e0a8fdd5ec05fb21bd9b379aa82488c179a0bb
Michael Pollmeier <michael@michaelpollmeier.com>
2020-09-07 14:56:43 +1200

    rename test

--- core/src/test/java/overflowdb/ElementTest.java
+++ core/src/test/java/overflowdb/ElementTest.java
@@ -21,7 +21,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class NodeDbTest {
+public class ElementTest {
 
   @Test
   public void overviewTest() {
