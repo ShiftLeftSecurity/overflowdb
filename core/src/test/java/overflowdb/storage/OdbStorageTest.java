804799627d6221dfe587d1546ca75e6b40201efc
Michael Pollmeier <michael@michaelpollmeier.com>
2020-10-27 11:23:33 +1300

    add glossary capability to storage

--- core/src/test/java/overflowdb/storage/OdbStorageTest.java
+++ core/src/test/java/overflowdb/storage/OdbStorageTest.java
@@ -131,5 +131,31 @@ public class OdbStorageTest {
     assertEquals(c, storage.reverseLookupStringToIntMapping(stringIdC));
   }
 
+  @Test
+  public void shouldProvideStringToIntGlossary() throws IOException {
+    File storageFile = Files.createTempFile("overflowdb", "bin").toFile();
+    storageFile.delete();
+    storageFile.deleteOnExit();
+    OdbStorage storage = OdbStorage.createWithSpecificLocation(storageFile, false);
+
+    String a = "a";
+    String b = "b";
+    Integer stringIdA = storage.lookupOrCreateGlossaryEntry(a);
+    Integer stringIdB = storage.lookupOrCreateGlossaryEntry(b);
+
+    // should be idempotent
+    assertEquals(stringIdA, storage.lookupOrCreateGlossaryEntry(a));
+    assertEquals(stringIdB, storage.lookupOrCreateGlossaryEntry(b));
+
+    // should survive restarts
+    storage.close();
+    storage = OdbStorage.createWithSpecificLocation(storageFile, false);
+    assertEquals(stringIdA, storage.lookupOrCreateGlossaryEntry(a));
+    assertEquals(stringIdB, storage.lookupOrCreateGlossaryEntry(b));
+
+    Integer stringIdC = storage.lookupOrCreateGlossaryEntry("c");
+    assertEquals(3, storage.getStringGlossary().size());
+  }
+
 
 }
