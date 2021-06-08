5d32327f9d9e5ef5a048326870b1107a57e0fd01
Michael Pollmeier <michael@michaelpollmeier.com>
2021-06-08 22:23:23 +1200

    add generic implementation of LowestCommonAncestor algorithm (#195)

---
+++ traversal/src/main/scala/overflowdb/algorithm/LowestCommonAncestors.scala
@@ -0,0 +1,36 @@
+package overflowdb.algorithm
+
+/**
+  * Find the lowest common ancestor(s)
+  *
+  * 1) for each relevant node, find their recursive parents
+  * 2) create the intersection of all of those sets
+  * 3) the LCA are those nodes, that do not have any children in that set
+  *
+  * based on https://www.baeldung.com/cs/lowest-common-ancestor-acyclic-graph
+  */
+object LowestCommonAncestors {
+
+  def apply[A](nodes: Set[A])(parents: A => Set[A]): Set[A] = {
+    def parentsRecursive(node: A): Set[A] = {
+      val nodeParents = parents(node)
+      nodeParents ++ nodeParents.flatMap(parentsRecursive)
+    }
+
+    if (nodes.size <= 1) {
+      nodes
+    } else {
+      val (head, tail) = (nodes.head, nodes.tail)
+      val parentsIntersection = tail.foldLeft(parentsRecursive(head)) {
+        case (res, next) =>
+          res.intersect(parentsRecursive(next))
+      }
+
+      parentsIntersection.filter { node =>
+        val childCount = parentsIntersection.count(parentsRecursive(_).contains(node))
+        childCount == 0
+      }
+    }
+  }
+
+}
