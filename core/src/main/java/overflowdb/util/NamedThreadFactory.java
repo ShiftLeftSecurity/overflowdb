72ead7be1bc15de104a7c3c9bd7aaa05e62e5ce0
Bernhard <bernhard@shiftleft.io>
2021-11-23 02:10:13 +0100

    minimal log message fix

--- core/src/main/java/overflowdb/util/NamedThreadFactory.java
+++ core/src/main/java/overflowdb/util/NamedThreadFactory.java
@@ -1,15 +1,19 @@
 package overflowdb.util;
 
+import org.slf4j.MDC;
+
+import java.util.Map;
 import java.util.concurrent.ThreadFactory;
 
 public class NamedThreadFactory implements ThreadFactory {
   private final String threadName;
+  private final Map<String, String> mdc = MDC.getCopyOfContextMap();
 
   public NamedThreadFactory(String threadName) {
     this.threadName = threadName;
   }
 
   public Thread newThread(Runnable r) {
-    return new Thread(r, threadName);
+    return new Thread(() -> {MDC.setContextMap(mdc); r.run();}, threadName);
   }
 }
