c65c61d54476818beddf9560c68ba742100ac5a1
Markus Lottmann <markus@shiftleft.io>
2021-12-03 17:46:16 +0100

    Use stripMargin on @Doc longInfo and example.
    We do this to remedy the fact that we cannot use stripMargin on the
    arguments of @Doc because it is now a Java annotation.

--- traversal/src/main/java/overflowdb/traversal/help/Doc.java
+++ traversal/src/main/java/overflowdb/traversal/help/Doc.java
@@ -3,6 +3,16 @@ package overflowdb.traversal.help;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+  * Annotation used for documentation.
+  *
+  * @param info a one line description for the overview table
+  * @param longInfo in-depth documentation processed by Scalas .stripMargin to
+  *                 allow for convienied multiline string writing with "|".
+  * @param example a short example for the overview table processed by Scalas
+  *                  .stripMargin to allow for convienied multiline string
+  *                  writing with "|".
+  * */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Doc {
   String info();
