689e01617d648c95d3d0bb51fab24187977dc119
Markus Lottmann <markus@shiftleft.io>
2021-12-03 19:38:55 +0100

    Factor out findDocumentedMethodsOf for reuse.

---
+++ traversal/src/main/scala/overflowdb/traversal/help/DocFinder.scala
@@ -0,0 +1,16 @@
+package overflowdb.traversal.help
+
+object DocFinder {
+  def findDocumentedMethodsOf(clazz: Class[_]): Iterable[StepDoc] = {
+    clazz.getMethods.flatMap { method =>
+      method.getAnnotations.find(_.isInstanceOf[Doc]).map { case docAnnotation: Doc =>
+        StepDoc(clazz.getName, method.getName,
+          StrippedDoc(docAnnotation.info, docAnnotation.longInfo.stripMargin, docAnnotation.example.stripMargin)
+        )
+      }
+    }
+  }
+
+  case class StepDoc(traversalClassName: String, methodName: String, doc: StrippedDoc)
+  case class StrippedDoc(info: String, longInfo: String, example: String)
+}
