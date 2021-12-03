e0f257938495278d701d73776540241e5b58f5e4
Markus Lottmann <markus@shiftleft.io>
2021-12-03 18:05:57 +0100

    Update traversal/src/main/java/overflowdb/traversal/help/Doc.java
    Co-authored-by: Michael Pollmeier <michael@michaelpollmeier.com>

--- traversal/src/main/java/overflowdb/traversal/help/Doc.java
+++ traversal/src/main/java/overflowdb/traversal/help/Doc.java
@@ -7,11 +7,11 @@ import java.lang.annotation.RetentionPolicy;
   * Annotation used for documentation.
   *
   * @param info a one line description for the overview table
-  * @param longInfo in-depth documentation processed by Scalas .stripMargin to
-  *                 allow for convienied multiline string writing with "|".
-  * @param example a short example for the overview table processed by Scalas
-  *                  .stripMargin to allow for convienied multiline string
-  *                  writing with "|".
+  * @param longInfo in-depth documentation
+  * @param example a short example for the overview table
+  * note: both longInfo and example are processed by Scala's `.stripMargin` to
+  *       allow for convenient multiline string writing with "|".
+  *       see https://docs.scala-lang.org/scala3/book/first-look-at-types.html#multiline-strings
   * */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Doc {
