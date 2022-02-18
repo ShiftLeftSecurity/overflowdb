f1888d9db69d424d455dee30647eda5db3264205
Michael Pollmeier <michael@michaelpollmeier.com>
2022-02-18 12:01:09 +0100

    fix test setup resource location

--- core-tests/src/test/scala/overflowdb/formats/GraphMLTests.scala
+++ core-tests/src/test/scala/overflowdb/formats/GraphMLTests.scala
@@ -10,7 +10,7 @@ class GraphMLTests extends AnyWordSpec {
     val graph = GratefulDead.newGraph()
     graph.nodeCount() shouldBe 0
 
-    GraphML.insert("formats/src/test/resources/graphml-small.xml", graph)
+    GraphML.insert("src/test/resources/graphml-small.xml", graph)
     graph.nodeCount() shouldBe 3
     graph.edgeCount() shouldBe 2
 
