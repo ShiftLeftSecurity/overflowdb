f3ff799a807495275b803ca03bd458faabee1ffc
bbrehm <bernhard@shiftleft.io>
2022-03-15 13:31:17 +0100

    Add defensive null-checks for MDC in order work around logback awesomeness (#293)
    * some versions of logback return null-pointer on getCopyOfContextMap() and then choke on setContextMap(null).

--- core/src/main/java/overflowdb/util/NamedThreadFactory.java
+++ core/src/main/java/overflowdb/util/NamedThreadFactory.java
@@ -2,15 +2,20 @@ package overflowdb.util;
 
 import org.slf4j.MDC;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ThreadFactory;
 
 public class NamedThreadFactory implements ThreadFactory {
   private final String threadName;
-  private final Map<String, String> mdc = MDC.getCopyOfContextMap();
+  private final Map<String, String> mdc;
 
   public NamedThreadFactory(String threadName) {
+
     this.threadName = threadName;
+    Map<String, String> mdcTmp = MDC.getCopyOfContextMap();
+    //logback chokes on null-maps
+    this.mdc = mdcTmp != null ? mdcTmp : new HashMap<>();
   }
 
   public Thread newThread(Runnable r) {
