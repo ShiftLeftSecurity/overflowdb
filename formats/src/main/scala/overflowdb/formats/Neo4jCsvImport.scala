bc8564899a0fcc5649dc5d4db424daea4fa45a0e
Michael Pollmeier <michael@michaelpollmeier.com>
2022-04-05 14:04:39 +0200

    skeleton for neo4jcsv tests

--- formats/src/main/scala/overflowdb/formats/Neo4jCsvImport.scala
+++ formats/src/main/scala/overflowdb/formats/Neo4jCsvImport.scala
@@ -4,7 +4,7 @@ import com.github.tototoshi.csv._
 import overflowdb.Graph
 import java.nio.file.Path
 
-object NeoCsvImport extends Importer {
+object Neo4jCsvImport extends Importer {
 
   override def runImport(inputFile: Path, graph: Graph): Unit = {
     ???
--- formats/src/test/scala/overflowdb/formats/NeoCsvTest.scala
+++ formats/src/test/scala/overflowdb/formats/NeoCsvTest.scala
@@ -2,7 +2,16 @@ package overflowdb.formats
 
 import org.scalatest.matchers.should.Matchers._
 import org.scalatest.wordspec.AnyWordSpec
+import overflowdb.testdomains.simple.SimpleDomain
 
-class NeoCsvTests extends AnyWordSpec {
+import java.nio.file.Paths
+
+class Neo4jCsvTests extends AnyWordSpec {
+  val neo4jcsvRoot = Paths.get("src/test/resources/neo4jcsv")
+
+  "foo" in {
+    val graph = SimpleDomain.newGraph()
+    Neo4jCsvImport.runImport(neo4jcsvRoot.resolve("testnodes"), graph)
+  }
 
 }
