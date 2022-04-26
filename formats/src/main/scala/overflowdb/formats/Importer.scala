8b8e84b6b3481373a9171abc0a48d32eddf4a0a3
Michael Pollmeier <michael@michaelpollmeier.com>
2022-04-26 10:50:31 +0200

    logger

--- formats/src/main/scala/overflowdb/formats/Importer.scala
+++ formats/src/main/scala/overflowdb/formats/Importer.scala
@@ -1,10 +1,13 @@
 package overflowdb.formats
 
+import org.slf4j.LoggerFactory
 import overflowdb.Graph
 
 import java.nio.file.Path
 
 trait Importer {
+  protected val logger = LoggerFactory.getLogger(getClass)
+
   def runImport(graph: Graph, inputFiles: Seq[Path]): Unit
 
   def runImport(graph: Graph, inputFile: Path): Unit =
