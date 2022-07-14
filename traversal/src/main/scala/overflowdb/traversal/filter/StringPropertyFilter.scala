07a27291deaff2d2c149d2458192e4d3ece80e68
Malte Kraus <mkraus@shiftleft.io>
2022-07-14 14:43:14 +0200

    StringPropertyFilter.exactMultiple helper for varargs queries
    mostly based on assumptions
    
    - use index if possible
    - use a mutable HashSet instead of immutable
    - Lazily create the HashSet only on the second iteration. Definitely a win when there's zero iterations. Since single-iteration is relatively common with `.where` and friends it seemed worth it to do delay set creation a bit more and take the hit do a single sequential scan.
    - I thought about not using sets at all when we only have a for very few values to search for but some small bit of microbenchmarking convinced me that's a bad idea.

--- traversal/src/main/scala/overflowdb/traversal/filter/StringPropertyFilter.scala
+++ traversal/src/main/scala/overflowdb/traversal/filter/StringPropertyFilter.scala
@@ -1,7 +1,9 @@
 package overflowdb.traversal.filter
 
-import java.util.regex.PatternSyntaxException
 import overflowdb.traversal.Traversal
+
+import java.util.regex.PatternSyntaxException
+import scala.collection.mutable
 import scala.util.matching.Regex
 
 object StringPropertyFilter {
@@ -39,8 +41,8 @@ object StringPropertyFilter {
   }
 
   /** compiles given string into a Regex which can be reused
-   * prefixes given string with `(?s)` to enable multi line matching
-   */
+    * prefixes given string with `(?s)` to enable multi line matching
+    */
   def regexpCompile(regexp: String): Regex =
     try {
       s"(?s)$regexp".r
@@ -67,5 +69,37 @@ object StringPropertyFilter {
   def endsWith[NodeType](trav: Traversal[NodeType])(accessor: NodeType => String,
                                                     value: String): Traversal[NodeType] =
     trav.filter(accessor(_).endsWith(value))
+
+  def exactMultiple[NodeType, ValueType](traversal: Traversal[NodeType],
+                                         accessor: NodeType => Option[ValueType],
+                                         needles: Seq[ValueType],
+                                         indexName: String): Traversal[NodeType] = {
+    if (needles.isEmpty)
+      return Traversal.empty
+
+    traversal match {
+      case init: overflowdb.traversal.InitialTraversal[NodeType] if init.canUseIndex(indexName) =>
+        needles
+          .to(Traversal)
+          .flatMap(needle => init.getByIndex(indexName, needle).get)
+      case _ =>
+        var iteration = 0
+        var needleSet: mutable.HashSet[ValueType] = null
+
+        traversal.filter { node =>
+          iteration += 1
+
+          accessor(node).exists { value =>
+            // Creating and accessing HashSets is expensive, so just sequentially scan needles for the first iteration.
+            // (A max of 1 iteration happens regularly with .where/.whereNot clauses.)
+            if (iteration >= 2 && needleSet == null)
+              needleSet = needles.to(mutable.HashSet)
+
+            if (needleSet == null) needles.contains(value)
+            else needleSet.contains(value)
+          }
+        }
+    }
+  }
 
 }
