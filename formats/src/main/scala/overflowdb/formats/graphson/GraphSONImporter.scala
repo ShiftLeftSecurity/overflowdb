a1e236de8edeb47dcad0e8a9296abe1d830e9035
David Baker Effendi <dbe@sun.ac.za>
2022-10-11 10:16:50 +0200

    flatMap -> mapValues

--- formats/src/main/scala/overflowdb/formats/graphson/GraphSONImporter.scala
+++ formats/src/main/scala/overflowdb/formats/graphson/GraphSONImporter.scala
@@ -10,10 +10,10 @@ import scala.io.Source.fromFile
 import scala.util.Using
 
 /**
- * Imports OverflowDB graph to GraphSON 3.0
- *
- * https://tinkerpop.apache.org/docs/3.4.1/dev/io/#graphson-3d0
- */
+  * Imports OverflowDB graph to GraphSON 3.0
+  *
+  * https://tinkerpop.apache.org/docs/3.4.1/dev/io/#graphson-3d0
+  */
 object GraphSONImporter extends Importer {
 
   override def runImport(graph: Graph, inputFiles: Seq[Path]): Unit = {
@@ -26,21 +26,24 @@ object GraphSONImporter extends Importer {
   }
 
   private def addNode(n: Vertex, graph: Graph): Unit = {
-    graph.addNode(n.id.`@value`, n.label, unboxProperties(n.properties): _*)
+    graph.addNode(n.id.`@value`, n.label, flattenProperties(n.properties): _*)
   }
 
-  private def unboxProperties(m: Map[String, Property]): Array[_] = {
-    m.flatMap {
-      case (k, v) => v.`@value` match {
-        case x: ListValue => Seq(k, x.`@value`.map(_.`@value`))
-        case x => Seq(k, x.`@value`)
+  private def flattenProperties(m: Map[String, Property]): Array[_] = {
+    m.view
+      .mapValues { v =>
+        v.`@value` match {
+          case x: ListValue => x.`@value`.map(_.`@value`)
+          case x            => x.`@value`
+        }
       }
-    }.toArray
+      .flatMap { case (k, v) => Seq(k, v) }
+      .toArray
   }
 
   private def addEdge(e: Edge, graph: Graph): Unit = {
     val src = graph.node(e.outV.`@value`)
     val tgt = graph.node(e.inV.`@value`)
-    src.addEdge(e.label, tgt, unboxProperties(e.properties): _*)
+    src.addEdge(e.label, tgt, flattenProperties(e.properties): _*)
   }
 }
